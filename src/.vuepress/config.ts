3801c0042b3aa23699037f5bea49d6c137718863
xiaoxunyao <huchengyea@163.com>
2022-01-30 19:49:44 +0800

    chorse: init

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,15 @@
+import { defineUserConfig } from 'vuepress'
+import type { DefaultThemeOptions } from 'vuepress'
+
+export default defineUserConfig<DefaultThemeOptions>({
+  // 站点配置
+  lang: 'en-US',
+  title: 'INet',
+  description: 'INet',
+
+  // 主题和它的配置
+  theme: '@vuepress/theme-default',
+  themeConfig: {
+    logo: '/img/logo.svg',
+  },
+})
