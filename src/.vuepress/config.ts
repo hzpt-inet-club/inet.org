9cd9d068b2ad74c90dcc4541005ed27e36014dda
INet <88970987+hzpt-inet@users.noreply.github.com>
2022-01-31 11:27:33 +0800

    docs: update docs

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,6 +2,7 @@ import { defineUserConfig } from 'vuepress'
 import type { DefaultThemeOptions } from 'vuepress'
 
 export default defineUserConfig<DefaultThemeOptions>({
+  base: '/inet.org/',
   // 站点配置
   lang: 'en-US',
   title: 'INet',
